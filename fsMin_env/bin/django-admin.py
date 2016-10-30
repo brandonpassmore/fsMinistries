ee3c99a56e29e51b64433648e21623b8ae2574c6
Brandon Passmore <brandon@fertilesoilministries.org>
2016-10-30 12:15:34 -0400

    Initial Fertile Soil Ministries Commit

---
+++ fsMin_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/brandon/HDD/Development/Django_Projects/fsMin/fsMin_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
